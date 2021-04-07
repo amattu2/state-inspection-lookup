68c2bee30bd6dcb44fb0d4cbd97bbac5c8669dd0
Alec M <alecmattu1234@gmail.com>
2021-04-07 19:50:36 -0400

    Setup MD/VA classes

--- classes/inspections.class.php
+++ classes/inspections.class.php
@@ -304,5 +304,77 @@ class MD implements StateInspectionInterface
     // Return
     return $parsed_records;
   }
+
+  /**
+   * @see StateInspectionInterface::fetch_emissions
+   * @see DOMDocument
+   */
+  public function fetch_emissions(string $VIN) : array
+  {
+    // Fetch Records
+    libxml_use_internal_errors(true);
+    $document = new \DOMDocument();
+    $records = InspectionHelper::http_post($this->endpoints["emissions"],
+      Array("vin" => $VIN)
+    ) ?: "";
+    $parsed_records = Array();
+
+    // Parse Results
+    $document->loadHTML($records);
+    echo $records;
+
+    // temp
+    return [];
+  }
+}
+
+/**
+ * Virginia State Inspection Wrapper
+ *
+ * @implements StateInspectionInterface
+ */
+class VA implements StateInspectionInterface
+{
+  // Class Variables
+  private $endpoints = Array();
+
+  /**
+   * @see StateInspectionInterface::fetch_all
+   */
+  public function fetch_all(string $VIN) : array
+  {
+    // Variables
+    $result = Array("Safety" => null, "Emissions" => null);
+
+    // Fetch Safety Inspection
+    try {
+      $result["Safety"] = $this->fetch_safety($VIN);
+    } catch (\Exception $e) {}
+
+    // Fetch Emissions Inspection
+    try {
+      $result["Emissions"] = $this->fetch_emissions($VIN);
+    } catch (\Exception $e) {}
+
+    // Return
+    return $result;
+  }
+
+  /**
+   * @see StateInspectionInterface::fetch_safety
+   */
+  public function fetch_safety(string $VIN) : array
+  {
+    throw new UnsupportedStateOperationException("This state does not support safety inspection results");
+  }
+
+  /**
+   * @see StateInspectionInterface::fetch_emissions
+   * @see DOMDocument
+   */
+  public function fetch_emissions(string $VIN) : array
+  {
+    throw new UnsupportedStateOperationException("This state does not support emissions results");
+  }
 }
 ?>
