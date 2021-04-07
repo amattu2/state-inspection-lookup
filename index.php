a94d46e29e2c39adbbf57eae170205d576fa02b6
Alec M <alecmattu1234@gmail.com>
2021-04-07 11:59:46 -0400

    Setup example inspection lookup

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+// Files
+require("classes/inspections.class.php");
+
+// Find Maryland inspections
+echo "<h1>Maryland Inspection Lookup</h1>", "<pre>";
+$records = amattu\Inspections::records("4JGBB8GB4BA662410", "MD");
+print_r($records);
+echo "</pre>";
