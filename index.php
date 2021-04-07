d0346ca65a24aa87f30d6d5bb5dcd35cfb75a32d
Alec M <alecmattu1234@gmail.com>
2021-04-07 17:31:53 -0400

    Update examples

--- index.php
+++ index.php
@@ -3,7 +3,9 @@
 require("classes/inspections.class.php");
 
 // Find Maryland inspections
-echo "<h1>Maryland Inspection Lookup</h1>", "<pre>";
-$records = amattu\Inspections::all("4JGBB8GB4BA662410", "MD");
+echo "<h1>Maryland Inspection Lookup</h1>", "<h2>Inspections</h2>", "<pre>";
+$records = amattu\Inspections::safety("4JGBB8GB4BA662410", "MD");
 print_r($records);
+echo "</pre>", "<h2>Emissions</h2>";
+$records = amattu\Inspections::emissions("1J8HG48K17C696395", "MD");
 echo "</pre>";
