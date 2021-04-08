7d67dc2a37c5b01d3138d8c1532f8fd9e9d773f2
Alec M <alecmattu1234@gmail.com>
2021-04-08 09:23:46 -0400

    Use Inspections::all instead of individual calls

--- index.php
+++ index.php
@@ -6,9 +6,11 @@ require("interfaces/StateInspection.interface.php");
 require("classes/states/MD.class.php");
 
 // Find Maryland inspections
+$all = amattu\Inspections::all("1J8HG48K17C696395", "MD");
 echo "<h1>Maryland Inspection Lookup</h1>", "<h2>Inspections</h2>", "<pre>";
-$records = amattu\Inspections::safety("4JGBB8GB4BA662410", "MD");
-print_r($records);
+//$records = amattu\Inspections::safety("4JGBB8GB4BA662410", "MD");
+print_r($all["Safety"]);
 echo "</pre>", "<h2>Emissions</h2>";
-$records = amattu\Inspections::emissions("1J8HG48K17C696395", "MD");
+//$records = amattu\Inspections::emissions("1J8HG48K17C696395", "MD");
+print_r($all["Emissions"]);
 echo "</pre>";
