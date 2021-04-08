bf10e8b203e6aba900e07e8b21e5c71810083737
Alec M <alecmattu1234@gmail.com>
2021-04-08 10:44:42 -0400

    Add missing pre tag

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ $all = amattu\Inspections::all("1J8HG48K17C696395", "MD");
 echo "<h1>Maryland Inspection Lookup</h1>", "<h2>Inspections</h2>", "<pre>";
 //$records = amattu\Inspections::safety("4JGBB8GB4BA662410", "MD");
 print_r($all["Safety"]);
-echo "</pre>", "<h2>Emissions</h2>";
+echo "</pre>", "<h2>Emissions</h2>", "<pre>";
 //$records = amattu\Inspections::emissions("1J8HG48K17C696395", "MD");
 print_r($all["Emissions"]);
 echo "</pre>";
