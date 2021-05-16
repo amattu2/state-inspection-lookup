c0acaa1782b38781dfb031f1ba615ebe57a821aa
Alec M <alecmattu1234@gmail.com>
2021-05-16 11:05:11 -0400

    Implement a dynamic lookup example page

--- index.php
+++ index.php
@@ -5,9 +5,12 @@ require("classes/StateInspection.class.php");
 require("interfaces/StateInspection.interface.php");
 require("classes/states/MD.class.php");
 
-// Find Maryland inspections
-$all = amattu\Inspections::all("1J8HG48K17C696395", "MD");
-echo "<h1>Maryland Inspection Lookup</h1>", "<h2>Inspections</h2>", "<pre>";
+// Find inspections
+$example_vin = $_GET["vin"] ?: "1J8HG48K17C696395";
+$example_state = $_GET["state"] ?: "MD";
+$all = amattu\Inspections::all($example_vin, $example_state);
+
+echo "<h1>Inspection Lookup</h1>", "<h2>Inspections</h2>", "<pre>";
 //$records = amattu\Inspections::safety("4JGBB8GB4BA662410", "MD");
 print_r($all["Safety"]);
 echo "</pre>", "<h2>Emissions</h2>", "<pre>";
