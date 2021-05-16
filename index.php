28f01a039c87fbdad59498084ef0ae20a050563c
Alec M <alecmattu1234@gmail.com>
2021-05-16 11:25:26 -0400

    Update example lookup page

--- index.php
+++ index.php
@@ -8,12 +8,24 @@ require("classes/states/MD.class.php");
 // Find inspections
 $example_vin = $_GET["vin"] ?: "1J8HG48K17C696395";
 $example_state = $_GET["state"] ?: "MD";
-$all = amattu\Inspections::all($example_vin, $example_state);
+$example_type = $_GET["type"] ?: "all";
 
-echo "<h1>Inspection Lookup</h1>", "<h2>Inspections</h2>", "<pre>";
-//$records = amattu\Inspections::safety("4JGBB8GB4BA662410", "MD");
-print_r($all["Safety"]);
-echo "</pre>", "<h2>Emissions</h2>", "<pre>";
-//$records = amattu\Inspections::emissions("1J8HG48K17C696395", "MD");
-print_r($all["Emissions"]);
-echo "</pre>";
+// Lookup Inspection Type
+if ($example_type == "all") {
+	$all = amattu\Inspections::all($example_vin, $example_state);
+	echo "<h1>Inspection Lookup</h1>", "<h2>Safety</h2>", "<pre>";
+	print_r($all["Safety"]);
+
+	echo "</pre>", "<h2>Emissions</h2>", "<pre>";
+	print_r($all["Emissions"]);
+	echo "</pre>";
+} else if ($example_type == "safety") {
+	echo "<h1>Inspection Lookup</h1>", "<h2>Safety</h2>", "<pre>";
+	$records = amattu\Inspections::safety($example_vin, $example_state);
+	print_r($records);
+} else if ($example_type == "emissions") {
+	echo "<h1>Inspection Lookup</h1>", "<h2>Emissions</h2>", "<pre>";
+	$records = amattu\Inspections::emissions($example_vin, $example_state);
+	print_r($records);
+	echo "</pre>";
+}
